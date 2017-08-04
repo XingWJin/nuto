6250d9294e68c4722f4a9332466a7b541364e061
Thomas Titscher <thomas.titscher@gmail.com>
2017-08-04 10:47:07 +0200

    :memo: Update Dockerfile to run nuto unit tests
    [ci skip]

--- docker/script.sh
+++ docker/script.sh
@@ -1,3 +1,3 @@
 sudo docker build -t nuto_docker .
-sudo docker tag nuto_docker nuto/nuto_docker:v1
-sudo docker push nuto/nuto_docker:v1
+sudo docker tag nuto_docker nuto/nuto_docker:v2
+sudo docker push nuto/nuto_docker:v2
