ea3e5b9a60720d416db9390931be4418bb7a2ca6
Thomas Titscher <thomas.titscher@googlemail.com>
2017-09-30 21:14:46 +0200

    Hopefully fix codecov with docker

--- scripts/upload_coverage.sh
+++ scripts/upload_coverage.sh
@@ -2,13 +2,13 @@
 set -ev
 if [[ "$COVERAGE" == "TRUE" ]]; then
      # get coverage information
-    docker exec docker_container lcov --capture --directory /build --output-file coverage.info
+    docker exec dock lcov --capture --directory build --output-file coverage.info
 
     # filter out system stuff that we don't control
     EXTERNALFILES=$(pwd)/external/*
-    docker exec docker_container lcov -r coverage.info '/usr/include/*' -o coverage.info
-    docker exec docker_container lcov -r coverage.info "$EXTERNALFILES" -o coverage.info
+    docker exec dock lcov -r coverage.info '/usr/include/*' -o coverage.info
+    docker exec dock lcov -r coverage.info "$EXTERNALFILES" -o coverage.info
 
     # upload to codecov
-    docker exec docker_container bash <(curl -s https://codecov.io/bash) -f coverage.info
+    docker exec dock bash <(curl -s https://codecov.io/bash) -f coverage.info
 fi
